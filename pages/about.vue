be57d96e6e276a08123ff250b300a9495a0b9cab
myles <hi@myles.glass>
2018-08-08 13:29:42 +1200

    configured about

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,24 @@
 <template>
-    <p>about</p>
+  <section class="section">
+    <div class="container">
+      <header>
+        <h1 class="title">{{ title }}</h1>
+        <p class="subtitle">We really out here</p>
+      </header>
+      <p class="paragraph">
+        {{ content }}
+      </p>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-    layout: 'default'
+    layout: 'default',
+
+    async asyncData() {
+        let content = await import('~/content/about.json')
+        return content
+    }
 }
 </script>

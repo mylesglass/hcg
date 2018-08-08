3b1a78190ac586afd9fb04cda0e2406e9732892e
myles <hi@myles.glass>
2018-08-08 22:47:16 +1200

    added members section

---
+++ pages/members/intro_to_comp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="intro">
+    <section class="section">
+      <div class="container">
+        <header>
+          <h1>{{ title }}</h1>
+          <p class="subtitle">{{ subtitle }}</p>
+        </header>
+        <span v-html="htmlContent"></span>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import Markdown from 'markdown-it'
+
+export default {
+  layout: 'default',
+
+  async asyncData() {
+    let content = await import('~/content/members/intro_to_comp.json')
+    return content
+  },
+
+  computed: {
+    htmlContent () {
+      let md = new Markdown({
+        breaks: true
+      })
+      
+      return md.render(this.markdown).replace(/<p>/g, '<p class="paragraph">')
+    }
+  }
+}
+</script>

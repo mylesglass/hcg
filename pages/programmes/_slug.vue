0d8b6939e29e26a9a4a6ee71403e86aa32b6a907
myles <hi@myles.glass>
2018-08-09 11:59:06 +1200

    we dynamic now

---
+++ pages/programmes/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <header>
+        <h1>{{ title }}</h1>
+      </header>
+    </div>
+  </section>
+</template>
+
+ <script>
+ export default {
+   async asyncData ({ params }) {
+     let post = await import('~/content/programmes/' + params.slug + '.json')
+     return post
+   }
+ }
+ </script>
+ 

970274ba031974c4ce5c1550c23a4556a1b3c4c4
myles <hi@myles.glass>
2018-08-19 00:09:35 +1200

    added vital info to programmes i.e. time days cost etc

---
+++ components/DayInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="tags has-addons">
+    <span class="tag" :class="{ 'is-primary': days.monday }">Mon</span>
+    <span class="tag" :class="{ 'is-primary': days.tuesday }">Tu</span>
+    <span class="tag" :class="{ 'is-primary': days.wednesday }">Wed</span>
+    <span class="tag" :class="{ 'is-primary': days.thursday }">Th</span>
+    <span class="tag" :class="{ 'is-primary': days.friday }">Fri</span>
+    <span class="tag" :class="{ 'is-primary': days.saturday }">Sat</span>
+    <span class="tag" :class="{ 'is-primary': days.sunday }">Sun</span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    days: Object
+  }
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- pages/programmes/_slug.vue
+++ pages/programmes/_slug.vue
@@ -5,6 +5,56 @@
         <h1>{{ title }}</h1>
         <p class="subtitle">{{ subtitle }}</p>
       </header>
+    </div>
+    <div class="container level-container" v-for="entry in classes" :key="entry.time" v-if="classes.length === 1">
+      <nav class="level">
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Days</p>
+            <day-info :days="entry.days[0]"/>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Time</p>
+            <p class="title">{{ entry.time }}</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <b-tooltip animated type="is-light" position="is-bottom" multilined label="This class runs for 10 weeks over the school term.">
+            <div>
+              <p class="heading">Cost</p>
+              <p class="title">{{ entry.cost }}</p>
+            </div>
+          </b-tooltip>
+        </div>
+        <div class="level-item has-text-centered" v-if="entry.detail">
+          <div>
+            <p class="heading">Info</p>
+            <p>{{ entry.detail }}</p>
+          </div>
+        </div>
+      </nav>
+    </div>
+    <div class="container" v-if="classes.length > 1">
+      <b-table :data="classes">
+        <template slot-scope="props">
+          <b-table-column field="days" label="Day">
+            <day-info :days="props.row.days[0]"/>
+          </b-table-column>
+          <b-table-column field="time" label="Time">
+            {{ props.row.time }}
+          </b-table-column>
+          <b-table-column field="cost" label="Cost">
+            {{ props.row.cost }}
+          </b-table-column>
+          <b-table-column field="detail" label="Details">
+            {{ props.row.detail }}
+          </b-table-column>
+        </template>
+      </b-table>
+    </div>
+    <div class="container">
       <span v-html="htmlContent" class="markdown"></span>
     </div>
     <div class="container" v-if="items">
@@ -16,6 +66,7 @@
  <script>
 import Markdown from 'markdown-it'
 import InfoCard from '~/components/InfoCard'
+import DayInfo from '~/components/DayInfo'
 
 export default {
   async asyncData ({ params }) {
@@ -24,7 +75,31 @@ export default {
   },
 
   components: {
-    InfoCard
+    InfoCard,
+    DayInfo
+  },
+
+  data () {
+    return {
+      columns: [
+        {
+          field: 'days',
+          label: 'Day'
+        },
+        {
+          field: 'time',
+          label: 'Time'
+        },
+        {
+          field: 'cost',
+          label: 'Cost'
+        },
+        {
+          field: 'detail',
+          label: 'Details'
+        }
+      ]
+    }
   },
 
   computed: {
@@ -45,4 +120,10 @@ export default {
   }
 }
  </script>
+ 
+ <style lang="scss" scoped>
+ .level-container {
+   padding-bottom: 2rem;
+ }
+ </style>
  

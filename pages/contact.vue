d40c6992a02e4644e1edbff3d32413ec77212856
myles <hi@myles.glass>
2018-09-16 00:08:31 +1200

    contact typo

--- pages/contact.vue
+++ pages/contact.vue
@@ -8,7 +8,7 @@
         </header>
         <div class="columns">
           <div class="column">
-            <p class="title">Phone</p>
+            <h2 class="title">Phone</h2>
             <a class="has-text-weight-bold is-size-5" :href="'tel:' + phone">{{ phone }}</a>
             <p class="paragraph">{{ phone_description }}</p>
             <ul>
@@ -17,8 +17,8 @@
             <p class="meta has-text-grey-light">{{ phone_disclaimer }}</p>
           </div>
           <div class="column">
-            <p class="title">Email</p>
-            <p class="paragraph">{{ email_desciption }}</p>
+            <h2 class="title">Email</h2>
+            <p class="paragraph">{{ email_description }}</p>
             <ul>
               <li v-for="item in email" :key="item.title">{{ item.title }}: <a :href="'mailto:' + item.url">{{ item.url }}</a></li>
             </ul>

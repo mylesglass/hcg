99c826fd54490ae42840ed479f4db5d730dd9a74
myles <hi@myles.glass>
2018-09-22 11:37:28 +1200

    fixed metadata and webfont import

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,28 +18,56 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'nuxt.js static site w/ netlify cms' },
+      { hid: 'description', name: 'description', content: 'Gymnastics provider fr all ages and abilities.' },
       { name: 'msapplication-TileColor', content: '#0e132b' },
       { name: 'msapplication-TileImage', content: '/favicon/ms-icon-144x144.png' },
       { name: 'theme-color', content: '#0e132b'}
     ],
     link: [
-      { 
-        rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico',
-        rel: 'apple-touch-icon', sizes: '57x57', href: '/favicon/apple-icon-57x57.png',
-        rel: 'apple-touch-icon', sizes: '60x60', href: '/favicon/apple-icon-60x60.png',
-        rel: 'apple-touch-icon', sizes: '72x72', href: '/favicon/apple-icon-72x72.png',
-        rel: 'apple-touch-icon', sizes: '76x76', href: '/favicon/apple-icon-76x76.png',
-        rel: 'apple-touch-icon', sizes: '114x114', href: '/favicon/apple-icon-114x114.png',
-        rel: 'apple-touch-icon', sizes: '120x120', href: '/favicon/apple-icon-120x120.png',
-        rel: 'apple-touch-icon', sizes: '144x144', href: '/favicon/apple-icon-144x144.png',
-        rel: 'apple-touch-icon', sizes: '152x152', href: '/favicon/apple-icon-152x152.png',
-        rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-icon-180x180.png',
-        rel: "icon", type: "image/png", sizes: "192x192",  href: "/android-icon-192x192.png",
-        rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png",
-        rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png",
-        rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png",
+      {
         rel: 'stylesheet', href: 'https://rsms.me/inter/inter-ui.css'
+      },
+      { 
+        rel: 'icon', type: 'image/x-icon', href: '/favicon/favicon.ico'
+      },
+      {
+        rel: 'apple-touch-icon', sizes: '57x57', href: '/favicon/apple-icon-57x57.png'
+      },
+      {
+        rel: 'apple-touch-icon', sizes: '60x60', href: '/favicon/apple-icon-60x60.png'
+      },
+      {
+        rel: 'apple-touch-icon', sizes: '72x72', href: '/favicon/apple-icon-72x72.png'
+      },
+      {
+        rel: 'apple-touch-icon', sizes: '76x76', href: '/favicon/apple-icon-76x76.png'
+      },
+      {
+        rel: 'apple-touch-icon', sizes: '114x114', href: '/favicon/apple-icon-114x114.png'
+      },
+      {
+        rel: 'apple-touch-icon', sizes: '120x120', href: '/favicon/apple-icon-120x120.png'
+      },
+      {
+        rel: 'apple-touch-icon', sizes: '144x144', href: '/favicon/apple-icon-144x144.png'
+      },
+      {
+        rel: 'apple-touch-icon', sizes: '152x152', href: '/favicon/apple-icon-152x152.png'
+      },
+      {
+        rel: 'apple-touch-icon', sizes: '180x180', href: '/favicon/apple-icon-180x180.png'
+      },
+      {
+        rel: "icon", type: "image/png", sizes: "192x192",  href: "/android-icon-192x192.png"
+      },
+      {
+        rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"
+      },
+      {
+        rel: "icon", type: "image/png", sizes: "96x96", href: "/favicon-96x96.png"
+      },
+      {
+        rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"
       }
     ]
   },

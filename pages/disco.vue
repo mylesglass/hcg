35dbcded75351eac6fe7c6ff3945f72af04e1022
Myles Glass <mylesalexanderglass@gmail.com>
2019-03-11 10:54:17 +1300

    Update and rename holprog.vue to disco.vue

---
+++ pages/disco.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="disco">
+    <div class="columns">
+      <div class="column is-half">
+        <img src="/images/uploads/disco_poster_2019.jpg" alt="">
+      </div>
+      <div class="column is-half">
+        <section class="section">
+          <div class="container">
+            <header>
+              <h1>Disco!</h1>
+              <p>Come along to the club to join us in dancing the night away! Pizza slices, lollies, and glow sticks will be available for purchase. You are welcome to bring your friends who do not attend gym.</p>
+            </header>
+            <p><strong>Saturday 30th March</strong></p>
+            <p><strong>6:15 - 8:30 PM</strong></p>
+            <p><strong>Entry $5 </strong></p>
+            <br/>
+            <p>
+              <a href="https://goo.gl/forms/185snuc76ajQpDgd2">Please fill out this form to secure your tickets!</a>
+              <br />
+              Pay by either:
+            </p>
+            <ul>
+              <li>Pay online into the HCG bank account 12-3174-0258558-00. Use Disco and your name as reference</li>
+              <li>Bringing cash on the night.</li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  padding-bottom: 2rem;
+}
+
+#holprog {
+  min-height: calc(100vh - 3.25rem);
+}
+
+a {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+
+</style>

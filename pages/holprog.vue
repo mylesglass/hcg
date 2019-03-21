61663aa2256a982d64d3f20ad85928a9386e342f
Myles Glass <mylesalexanderglass@gmail.com>
2019-03-21 15:15:09 +1300

    Create holprog.vue

---
+++ pages/holprog.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="disco">
+    <div class="columns">
+      <div class="column is-half">
+        <img src="/images/uploads/holprog-t1-19-v3.jpg" alt="">
+      </div>
+      <div class="column is-half">
+        <section class="section">
+          <div class="container">
+            <header>
+              <h1>Holiday Programme!</h1>
+              <p>
+                Harbour City GymSports is hosting an easter holiday programme over the term 1 break! Come along to join all sorts of exciting
+                action-packed activities such as gymnastics, arts & crafts, fun games, fort building, and free time in the gym!
+                Spaces are limited so get in quick!
+              </p>
+            </header>
+            <p><strong>Monday 15th April</strong></p>
+            <p><strong>Tuesday 16th April</strong></p>
+            <p><strong>Wednesday 17th April</strong></p>
+            <p><strong>9:00 AM - 3:00 PM</strong></p>
+            <p><strong>$35 per day</strong></p>
+            <br/>
+            <p>
+              <a href="https://goo.gl/forms/jzKIcniw0ioE6U5v1">Please fill out this form to register your gymnast!</a>
+            </p>
+            <p>Pay online into the HCG bank account 12-3174-0258558-00.</p>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  padding-bottom: 2rem;
+}
+
+#holprog {
+  min-height: calc(100vh - 3.25rem);
+}
+
+a {
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+}
+
+</style>

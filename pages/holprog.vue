c26666799e535e77747d88b8a0679a336f777986
mylesglass <mylesalexanderglass@gmail.com>
2019-06-14 10:57:16 +1200

    holiday programme updates for term 2

--- pages/holprog.vue
+++ pages/holprog.vue
@@ -1,39 +1,8 @@
 <template>
   <div id="holprog">
-    <img src="/images/svg/easter-egg-1.svg" class="egg egg1">
-    <img src="/images/svg/easter-egg-2.svg" class="egg egg2">
-    <img src="/images/svg/easter-egg-3.svg" class="egg egg3">
-    <img src="/images/svg/easter-egg-4.svg" class="egg egg4">
-    <img src="/images/svg/easter-egg-5.svg" class="egg egg5">
     <section class="section">
-      <div class="container main-container">
-        <figure class="image">
-          <img src="/images/uploads/header.png" alt="Easter Holiday Programme" class="title-image">
-        </figure>
-        <p class="blurb">
-          Harbour City GymSports is hosting an Easter holiday programme over the Term 1 break!
-        </p>
 
-        <p class="blurb">
-          <strong>Monday 15th April</strong><br/>
-          <strong>Tuesday 16th April</strong><br/>
-          <strong>Wednesday 17th April</strong>
-        </p>
-
-        <p class="has-text-centered">
-          Each day will be action-packed with lots of exciting activities such as gymnastics, 
-          games, arts & crafts, fort building, and free time in the gym.<br>Each day will cost <strong>$35</strong> and run from
-          <strong>9 AM - 3 PM</strong>.
-        </p>
-        <br/>
-        <nav class="level">
-          <p class="level-item">
-            <a class="button is-medium is-primary is rounded" href="https://goo.gl/forms/jzKIcniw0ioE6U5v1">
-              Click Here to Register
-            </a>
-          </p>
-        </nav>
-      </div>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdsrbgSsm3bsCbCLAm7qXuBndY1KjZSIVXu3Bzl_faa3LX_QA/viewform?embedded=true" width="100%" height="1677" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
     </section>
     
   </div>
@@ -49,59 +18,11 @@ export default {
 
 #holprog {
   background-color: #ACD6F3;
-  min-height: 100vh;
+  
 }
 
-.title-image {
-  z-index: 1;
-}
-
-.main-container {
-  max-width: 750px;
-}
-
-.blurb {
-  font-size: 1.6rem;
-  text-align: center;
-  line-height: 2rem;
-  color: #262262;
-  font-weight: 500;
-  padding-bottom: 2rem;
-}
-
-.egg {
-  position: absolute;
-  z-index: 0;
-}
-
-.egg1 {
-  width: 12vw;
-  top: 10vh;
-  left: 10vw;
-}
-
-.egg2 {
-  width: 12vw;
-  top: 10vh;
-  right: 10vw;
-}
-
-.egg3 {
-  width: 12vw;
-  bottom: 10vh;
-  left: 20vw;
-}
-
-.egg4 {
-  width: 12vw;
-  bottom: 10vh;
-  right: 10vw;
-}
-
-.egg5 {
-  width: 7vw;
-  bottom: 5vh;
-  left: 40vw;
+.section {
+  padding: 1rem 1rem;
 }
 
 </style>

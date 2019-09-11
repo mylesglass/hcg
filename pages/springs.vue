922d70412504252e1d4e936ca5b30826c15fcc49
mylesglass <mylesalexanderglass@gmail.com>
2019-09-11 15:31:49 +1200

    added basic springs page

---
+++ pages/springs.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="springs">
+    <section class="section">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdEFHn8PwGsjd3r5cNJXfdkQrSeG5LIgU53lueYpvCCpcVlAQ/viewform?embedded=true" width="100%" height="1693" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </section>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+#springs {
+  background-color: #ACD6F3;
+  
+}
+
+.section {
+  padding: 1rem 1rem;
+}
+
+</style>

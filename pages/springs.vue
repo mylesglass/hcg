e9f694e09f834cab0a138c888395326dd32c4203
mylesglass <mylesalexanderglass@gmail.com>
2019-10-23 14:47:57 +1300

    Spring Fundraiser Updated

--- pages/springs.vue
+++ pages/springs.vue
@@ -1,9 +1,60 @@
 <template>
-  <div id="springs">
+  <div class="container">
     <section class="section">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdEFHn8PwGsjd3r5cNJXfdkQrSeG5LIgU53lueYpvCCpcVlAQ/viewform?embedded=true" width="100%" height="2300px" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <div class="columns">
+        <div class="column">
+          <header>
+            <h1>
+              Spring Floor Fundraiser
+            </h1>
+            <p class="subtitle">
+              Our spring floor is getting a little bit old, and starting to lose some of it's spring! We're asking our community for help to get the bounce back. 
+            </p>
+          </header>
+          <img src="~/assets/spring-image-header.jpg" alt="Spring Fundraiser!">
+          <br>
+          <p>
+            The floor is one of the main pieces of equipment we use in gymnastics. All classes currently run within the gym work on the floor. It's a bit of the hard side, and if we can do a bit of renovation to increase the floors spring we'll be able to utilise it even more! Creating a surface with more rebound will allow our gymnasts to learn more difficult skills - while lessening the risk of injury through overuse.
+          </p>
+          <br>
+          <div class="columns">
+            <div class="column">
+              <p>
+                The floor is made up of 1600 springs covered with plywood and foam. The plywood and foam are still in great condition - so instead of replacing the whole thing we just need to replace the springs.
+                <br/><br/>
+                We're thinking that since <i>many hands make light work</i>, if we can get our extended gymnastics community to help chip in a purchase a spring or two, we'll be able to get the springs shipped here from USA over the summer break, and have a new floor ready for 2020!
+                <br/><br/>
+                <strong>Fill out the form on this page to purchase a spring (or 10)</strong>
+              </p>
+            </div>
+            <div class="column">
+              <figure >
+                <img src="~/assets/spring-floor-illustration.png" alt="Spring Floor Illustration" width="">
+              </figure>
+            </div>
+          </div>
+          <div class="markdown">
+            <h2>Recognition Board</h2>
+            <p>
+              You may have noticed the fantastic recognition board we've got up on our mezzanine. We're using it as a visual way to recognise who has helped us to achieve this goal. Every spring purchased has a name attached to it - we're hoping to have it full by the end of the year!
+              <br><br>
+              A huge thanks to Ruth & Scott for all thier hard work with designing, creating, and managing the board!
+            </p>
+            <br>
+            <p>
+              <strong>Board Sponsors</strong>
+              <br/>
+              Any group, family, or business that purchases a lot of 32 springs - get a special panel of recognition on our board. You can make it your logo, your family name, or a picture of your dog*
+              
+            </p>
+            <span class="meta">*must meet design guidelines</span>
+          </div>
+        </div>
+        <div class="column">
+          <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdEFHn8PwGsjd3r5cNJXfdkQrSeG5LIgU53lueYpvCCpcVlAQ/viewform?embedded=true" width="100%" height="2300px" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
+      </div>
     </section>
-    
   </div>
 </template>
 
@@ -14,14 +65,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
-#springs {
-  background-color: #ACD6F3;
-  
+header {
+  padding-bottom: 2rem;
 }
-
-.section {
-  padding: 3rem 1rem;
-}
-
 </style>

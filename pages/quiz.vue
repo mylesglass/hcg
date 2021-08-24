121b624a128c069e1b5497087a6af6e627798d55
Myles Glass <mylesalexanderglass@gmail.com>
2021-08-24 13:45:37 +1200

    Create quiz.vue

---
+++ pages/quiz.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="holprog">
+    <section class="section">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeYDC6hoTd0fLwYYlpb4xGFM9Oh7rqhhP7LCcoq_-K-H6Y-Vg/viewform?embedded=true" width="100%" height="1693" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </section>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+#holprog {
+  background-color: #ACD6F3;
+  
+}
+
+.section {
+  padding: 1rem 1rem;
+}
+
+</style>

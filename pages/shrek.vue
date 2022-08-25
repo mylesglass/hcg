41a87565edd364a7391a5afb73c8ce839a918531
Myles Glass <mylesalexanderglass@gmail.com>
2022-08-25 22:33:10 +1200

    Create shrek.vue

---
+++ pages/shrek.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="shrek">
+    <section class="section">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScS5O2OjAbwZSGKT1mq0t7p-ocqj27ISLjzx-bnp42Wkg-LsA/viewform?embedded=true" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+#shrek {
+  background-color: #7A9712;
+  
+}
+.section {
+  padding: 1rem 1rem;
+}
+</style>

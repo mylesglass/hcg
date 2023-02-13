cc5899c622047fe3da1978964e64fa5b73288455
Myles Glass <mylesalexanderglass@gmail.com>
2023-02-13 13:31:56 +1300

    Create handbalancing.vue

---
+++ pages/handbalancing.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="holprog">
+    <section class="section">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe7zqxPbSp3Z4vHd5p12BN9Z7gH0-RAsNye50Ec9rL0bqmvlw/viewform?embedded=true" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+#holprog {
+  background-color: #ACD6F3;
+  
+}
+.section {
+  padding: 1rem 1rem;
+}
+</style>

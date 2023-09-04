42c56f9a969969b8209b57e2bde47d629f7c899d
Myles Glass <mylesalexanderglass@gmail.com>
2023-09-05 09:46:24 +1200

    Create rat.vue

---
+++ pages/rat.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="movie">
+    <section class="section">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSehTTaN2wYoI85OABuTzyiNWt-gAo_A_r6baRgNlik5464SAg/viewform?embedded=true" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+#movie {
+  background-color: #F9CBD6;
+  
+}
+.section {
+  padding: 1rem 1rem;
+}
+</style>

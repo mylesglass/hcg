f1a46e5b80313f8c51e1004ca63806bd684edf5f
Myles Glass <mylesalexanderglass@gmail.com>
2023-09-05 09:47:39 +1200

    Update rat.vue

--- pages/rat.vue
+++ pages/rat.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="movie">
     <section class="section">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSehTTaN2wYoI85OABuTzyiNWt-gAo_A_r6baRgNlik5464SAg/viewform?embedded=true" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSewozD4zcSQ3Uu_uibdqzYTTDkUG-xzPHOw5e60JJOJN5uczQ/viewform?embedded=true" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </section>
   </div>
 </template>
@@ -14,7 +14,7 @@ export default {
 
 <style lang="scss" scoped>
 #movie {
-  background-color: #F9CBD6;
+  background-color: #174583;
   
 }
 .section {

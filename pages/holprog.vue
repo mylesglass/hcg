050f849542120df60abe31adf1137481e4e2e5a0
Myles Glass <mylesalexanderglass@gmail.com>
2024-03-20 16:42:04 +1300

    Update holprog.vue

--- pages/holprog.vue
+++ pages/holprog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="holprog">
     <section class="section">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdacegnnAAEV-XbVjzMd8d7jSOIRIFVpBqKjA1ZYlQlPKOhuQ/viewform?embedded=true" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSejTQVdjeYBRaFpHKlXSwqrok4kD-PaHJfqC5rgfz7pG0anpA/viewform?embedded=true" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </section>
   </div>
 </template>

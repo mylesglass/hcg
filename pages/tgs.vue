b064ec2a8892d08d72b7c9880698eab030b966f2
Myles Glass <mylesalexanderglass@gmail.com>
2025-05-21 12:10:14 +1200

    Create tgs.vue

---
+++ pages/tgs.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="movie">
+    <section class="section">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfEr--Fh9XJJrpmnkY0uVvsTTRzTT9hXzHQm10JKMOh6XOVwg/viewform?embedded=true" width="100%" height="900" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+i    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+#movie {
+  background-color: #233D77;
+  
+}
+.section {
+  padding: 1rem 1rem;
+}
+</style>

ea3f0791a40d670586715cf925f2ab769f6f2cd1
Myles Glass <mylesalexanderglass@gmail.com>
2025-05-21 12:50:09 +1200

    Create hoodies.vue

---
+++ pages/hoodies.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="holprog">
+    <section class="section">
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe6NoVubzgbaE9KwBv-OrzxKBiF1Ny_6s9TgcJw1Lzp_vlYrA/viewform?embedded=true" width="100%" height="1693" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+    </section>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+#holprog {
+  background-color: #FFFFFF;
+  
+}
+
+.section {
+  padding: 1rem 1rem;
+}
+
+</style>

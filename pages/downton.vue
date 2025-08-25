f01f1aeafd4afbf15ba95d977eb1f337a1f4e3f0
Myles Glass <mylesalexanderglass@gmail.com>
2025-08-25 15:28:50 +1200

    Create downton.vue

---
+++ pages/downton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="movie">
+    <section class="section">
+       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSe_e775B1Eu-1kQzDCl0sGuDeq8HR4E2rKyfAZkO0ywqWpBJg/viewform?embedded=true" width="100%" height="1385" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>   
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+#movie {
+  background-color: #233D77;
+  
+}
+.section {
+  padding: 1rem 1rem;
+}
+</style>

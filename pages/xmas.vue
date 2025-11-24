a1a46b13e1d24d1015e78041dc5fdb74cfdcf22c
Myles Glass <mylesalexanderglass@gmail.com>
2025-11-24 14:58:26 +1300

    Add xmas.vue for holiday program display

---
+++ pages/xmas.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="holprog">
+    <section class="section">
+      <script src="https://harbourcitygym.friendlymanager.com/js/loadform/holidayprogram.js"></script>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss" scoped>
+
+#holprog {
+  background-color: #de312f;
+  
+}
+
+.section {
+  padding: 1rem 1rem;
+}
+
+</style>

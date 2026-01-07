51d511517342a83b1750e7c436b7b211b5d0190b
Myles Glass <mylesalexanderglass@gmail.com>
2026-01-07 18:33:02 +1300

    Replace iframe with holiday program loading script
    Replaced Google Forms iframe with a script to load the holiday program.

--- pages/holprog.vue
+++ pages/holprog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="holprog">
     <section class="section">
-      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSd1fwTWUlztxJz8KuUl6UTF10XY-CwyhWrtxrZpzWABqJvz1Q/viewform?embedded=true" width="100%" height="2051" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+      <script src="https://harbourcitygym.friendlymanager.com/js/loadform/holidayprogram.js"></script>
     </section>
   </div>
 </template>
